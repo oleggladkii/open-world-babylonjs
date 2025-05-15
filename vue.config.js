7bd92fa37e1b666b96a369232a3c6d2899ff6482
oleggladkii <oleg.gladkii@onix-systems.com>
2025-05-15 14:40:37 +0300

    Removed Docker-related files including `Dockerfile` and `docker-compose.yml` to streamline the project setup. Adjusted `vue.config.js` to remove the `prerender-spa-plugin-next` configuration, and cleaned up Sentry integration in `main.ts` for improved clarity and maintainability.

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,6 @@ const plugins = [];
 
 if (process.env.NODE_ENV === "production") {
   const { join } = require("path");
-  const PrerenderPlugin = require("prerender-spa-plugin-next");
-
-  plugins.unshift(
-    new PrerenderPlugin({
-      staticDir: join(__dirname, "dist"),
-      routes: ["/"],
-      renderAfterDocumentEvent: "render-event",
-    })
-  );
 }
 
 module.exports = defineConfig({
